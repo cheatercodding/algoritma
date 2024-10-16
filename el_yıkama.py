0a2b6849bbfcc6825e71c18ed6dc966273713f1b
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-16 15:43:51 +0300

    burda el yıkama ile ilgii bir algoritma yazdım

---
+++ el_yıkama.py
@@ -0,0 +1,10 @@
+
+def el_yikama():
+    print("El yıkama adımlarını takip edin:")
+    print("1. Ellerini su ile ıslat.")
+    print("2. Sabun kullan.")
+    print("3. Ellerini fırçala.")
+    print("4. Durula.")
+    print("5. Kurula.")
+    # Fonksiyonu bir kez çağır
+    el_yikama()

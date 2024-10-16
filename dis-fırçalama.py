f8a35ac1cfa0c09f4b470661ef69348b0b6369d5
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-16 15:13:29 +0300

    burda basit bir tane dis fırçalama uygulaması yaptım

---
+++ dis-fırçalama.py
@@ -0,0 +1,10 @@
+def dis_fırcalama():
+    print("Diş fırçalama uygulamasını hoş geldiniz!")
+    input("Diş fırçanızı ve diş macununuzu hazırlayın. Devam etmek için Enter'a basın..")
+    print("Diş macununu fırçanın üstüne koyun.")
+    input("dişlerinizi fırçlamaya başlayın. Devam etmek için enter'a basın... ")
+    print("Dişlerinizi 2 dakkika fırçalayın.")
+    input("Fırçalamayı bitirdikten sonra ağzınızı durulayın. Devam etmek için Enter'a basın...")
+    print("Diş fırçalama işlemi tamamlandı!")
+    # Uygulamayı başlat
+    dis_fırcalama()

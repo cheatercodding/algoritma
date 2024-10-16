dacb384c4e403746b2d8803a24bced4f8aaefd3f
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-16 15:46:12 +0300

    yemek masası hazırlma algoritması yaptım

---
+++ yemek_masası_hazırlama.py
@@ -0,0 +1,11 @@
+def yemek_masasi_hazirla():
+    print("Yemek masası hazırlama işlemine başla.")
+    print("Masa örtüsünü masaya ser.")
+    print("Tabakları yerleştir.")
+    print("Çatal, bıçak ve kaşıkları yerleştir.")
+    print("Su bardaklarını masaya koy.")
+    print("Yemekleri servis için hazırla.")
+    print("Yemek masası hazırlama işlemi tamamlandı.")
+
+# Fonksiyonu bir kez çağır
+yemek_masasi_hazirla()
